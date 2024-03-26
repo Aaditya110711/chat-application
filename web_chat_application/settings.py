19d2357af7b1b730fbb81ce1c0464665ca208b05
Aaditya110711 <139574062+Aaditya110711@users.noreply.github.com>
2024-03-26 15:56:53 +0530

    Update settings.py

--- web_chat_application/settings.py
+++ web_chat_application/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(o&!12ga85rk+hwm+xe7t$&h^sqv2s$gpz%j^&vq%7cfbb1382'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
